2400f60fcd19c56e5b32df3dbf98c6269ab12e38
TRUNG_mainpc <nguy.hoangtrung@gmail.com>
2025-10-28 16:44:44 +0100

    fulfills #2

--- script.py
+++ script.py
@@ -1,6 +1,10 @@
 # MVP for FilmCriticLM
 
 import random
+import re
+from numpy import dot
+from numpy.linalg import norm
+from sklearn.feature_extraction.text import CountVectorizer
 
 data = []
 
@@ -20,27 +24,52 @@ def read_data():
 def run_training():
     pass
 
+def sentence_to_list(sentence):
+    return sentence.split(" ")
+
+def list_to_sentence(list):
+    result = ""
+    for i in range(len(list)):
+        if i == len(list) - 1:
+            result += list[i]
+        else:
+            result += list[i] + " "
+    return result
+
 def run_evaluation():
-    accuracy = 0
-    creativity = 0
+    accuracy_sum = 0
+    # creativity_sum = 0
     num = min(len(data)-1, 100)
     for i in range(num):
-        test_prompt = data[i].split(" ")[:-2]
+        test_prompt = list_to_sentence(sentence_to_list(data[i])[:-2])
+        test_expected_answer = list_to_sentence(data[i].split(" ")[-2:])
         test_response = generate_response(test_prompt)
 
-    print("Accuracy:", accuracy)
-    print("Creativity:", creativity)
+        print("Prompt:", test_prompt)
+        print("Expected answer:", test_expected_answer)
+        print("Actual answer:", test_response)
+
+        manual_test = int(input("Valid (1) or not (0)?"))
+        if manual_test == 1:
+            accuracy_sum += 1
+        print("\n")
+
+    print("Accuracy:", float(accuracy_sum/num))
+    # print("Creativity:", float(creativity_sum/num))
     print("Evaluation complete.\n")
 
 def generate_response(prompt):
     # pick random data entry
+    assert isinstance(prompt, str), "Prompt must be a string!"
     result = data[random.randint(0, len(data) - 1)]
-    assert isinstance(result, str), "Generated response must be a string."
+    assert isinstance(result, str), "Generated response must be a string!"
 
-    print(result + "\n")
-    print("Response generated.\n")
+    return result
+
 
 def run_tests():
+    assert sentence_to_list("This is a test") == ["This", "is", "a", "test"], "Converting sentences to lists failed!"
+    assert list_to_sentence(["This", "is", "a", "test"]) == "This is a test", "Converting lists to sentences failed!"
     print("Tests passed.\n")
 
 # main
@@ -59,6 +88,7 @@ if __name__ == '__main__':
             run_evaluation()
         case 3:
             prompt = input("Requesting prompt...\n")
-            generate_response(prompt)
+            print(generate_response(prompt) + "\n")
+            print("Response generated.\n")
         case _:
             print("This is not a valid input.")
