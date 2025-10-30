99ab1fd01b80018961c9f6c9cbf986b2808939e7
finn <finn.eilmann@web.de>
2025-10-30 19:05:59 +0100

    added Makefile for automated download and filtering

--- src/data-filter.py
+++ src/data-filter.py
@@ -1,7 +1,15 @@
+import sys
+import os
 import json
 
-input_file = "../data/letterboxd_full.jsonl"   # your original file
-output_file = "../data/letterboxd_filtered.jsonl"  # the filtered file
+if len(sys.argv) < 2:
+    print("usage: python data-filter.py <input_file>")
+    sys.exit(1)
+
+#input_file = "../data/letterboxd_full.jsonl"   # hardcoded
+#output_file = "../data/letterboxd_filtered.jsonl"  # hardcoded
+input_file = sys.argv[1]
+output_file = os.path.join(os.path.dirname(input_file), "letterboxd_filtered.jsonl")
 
 with open(input_file, "r", encoding="utf-8") as infile, open(output_file, "w", encoding="utf-8") as outfile:
     for line in infile:
@@ -10,7 +18,6 @@ with open(input_file, "r", encoding="utf-8") as infile, open(output_file, "w", e
             "title": data.get("title"),
             "year": data.get("year"),
             "synopsis": data.get("synopsis"),
-            #"reviews": data.get("reviews")
             "review_texts": [r.get("review_text", "") for r in data.get("reviews", [])]
         }
         outfile.write(json.dumps(filtered, ensure_ascii=False) + "\n")
