f6e437b8ffd4e50211a8157b7dffa08aa215cb0d
Till <till.flotow@gmx.de>
2025-11-13 14:55:29 +0100

    [feature/embedding] cleaned __init__

--- src/scripts/__init__.py
+++
@@ -1,4 +0,0 @@
-from src.tokenizer.char_tokenizer import CharTokenizer
-import os
-os.chdir('/Users/till/workspaces/The-TruFiTi-Group/')
-tokenizer = CharTokenizer.load('tokenizer/char_tokenizer.json')
