5fc542c7256107fbcfe2b8ad9a076cc330ff07cd
TRUNG_mainpc <nguy.hoangtrung@gmail.com>
2025-11-14 01:24:12 +0100

    fixed merge conflicts

--- src/tokenizer/bpe_tokenizer.py
+++ src/tokenizer/bpe_tokenizer.py
@@ -1,23 +1,145 @@
+import json
+
 from src.tokenizer.abstract_tokenizer import AbstractTokenizer
 
 
 class BPETokenizer(AbstractTokenizer):
+    BASE_VOCAB_SIZE = 256
+    vocabulary_size = BASE_VOCAB_SIZE
+
     def __init__(self):
         super().__init__()
+        self._vocabulary = self._return_base_vocabulary()
+        self._merge_rules = []
+
+    def _return_base_vocabulary(self) -> dict:
+        """
+        Returns a dictionary that is the basis for the vocabulary of a BPETokenizer.
+        """
+        result = dict()
+
+        for i in range(256):
+            result[i] = i.to_bytes()
+
+        return result.copy()
+
+    def _is_prefix_of(self, list1, list2) -> bool:
+        """
+        Returns True if list1 is a prefix of list2.
+        """
+        if len(list1) <= len(list2):
+            return list2[:len(list1)] == list1
+        else:
+            return False
+
+    def _apply_merge_rule(self, tokens: list[int], rule) -> list[int]:
+        """
+        Applies merge rules to a list of tokens.
+        """
+        result = []
+        i = 0
+        while i < len(tokens):
+            if i < len(tokens)-1:
+                if (tokens[i] == rule[0]) and (tokens[i+1] == rule[1]):
+                    result.append(rule[2])
+                    i +=2
+                else:
+                    result.append(tokens[i])
+                    i += 1
+            else:
+                result.append(tokens[i])
+                i += 1
+
+        return result.copy()
 
     def encode(self, text: str) -> list[int]:
-        raise NotImplementedError()
+        """
+        Encodes a string into a list of integers.
+        """
+        result = []
+        input_bytes = text.encode("utf-8")
+
+        i = 0
+        while i < len(input_bytes):
+            temp = -1
+            temp_len = 0
+            for key, value in self._vocabulary.items():
+                if self._is_prefix_of(value, input_bytes[i:]) and (key > temp):
+                    temp = key
+                    temp_len = len(value)
+            result.append(temp)
+            i += temp_len
+
+        return result.copy()
 
     def decode(self, tokens: list[int]) -> str:
-        raise NotImplementedError()
+        """
+        Decodes tokens and returns a string.
+        """
+        result = bytes("", "utf-8")
+
+        for token in tokens:
+            result += self._vocabulary[token]
+
+        return result.decode("utf-8")
+
+    @classmethod
+    def set_vocabulary_size(cls, size: int) -> None:
+        cls.vocabulary_size = size
 
     @classmethod
     def train(cls, texts: list[str]) -> 'BPETokenizer':
-        raise NotImplementedError()
+        tokenizer = cls()
+
+        all_words = list()
+        for text in texts:
+            words = text.strip().split(" ")
+            for word in words:
+                all_words.append(tokenizer.encode(word))
+
+        for i in range(cls.BASE_VOCAB_SIZE, cls.vocabulary_size):
+
+            counter = {}
+
+            for word in all_words:
+                for j in range(len(word) - 1):
+                    pair = str(word[j]) + "-" + str(word[j + 1])  # necessary because lists are not hashable in Python
+                    if pair in counter:
+                        counter[pair] += 1
+                    else:
+                        counter[pair] = 1
+
+            dominant = max(counter, key=counter.get)
+            dominant_pair = dominant.split("-")
+            first_bytes = tokenizer._vocabulary[int(dominant_pair[0])]
+            second_bytes = tokenizer._vocabulary[int(dominant_pair[1])]
+            byte_pair = first_bytes + second_bytes
+
+            last_index = len(tokenizer._vocabulary)
+            tokenizer._vocabulary[last_index] = byte_pair
+            new_rule = [int(dominant_pair[0]), int(dominant_pair[1]), last_index]
+            tokenizer._merge_rules.append(new_rule)
+
+            for k in range(len(all_words)):
+                all_words[k] = tokenizer._apply_merge_rule(all_words[k], new_rule)
+
+        print(tokenizer._vocabulary)
+        print(tokenizer._merge_rules)
+
+        return tokenizer
 
     def save(self, path: str):
-        raise NotImplementedError()
+        with open(path, 'w') as f:
+            json.dump({
+                'vocabulary': self._vocabulary,
+                'merge_rules': self._merge_rules
+            }, f)
 
     @classmethod
     def load(cls, path: str) -> 'BPETokenizer':
-        raise NotImplementedError()
+        tokenizer = cls()
+        with open(path, 'r') as f:
+            data = json.load(f)
+            tokenizer._vocabulary = data['vocabulary']
+            tokenizer._merge_rules = data['merge_rules']
+        return tokenizer
---
+++ tests/test_bpe_tokenizer.py
@@ -0,0 +1,19 @@
+import pytest
+
+from src.tokenizer.BPETokenizer import BPETokenizer
+
+@pytest.fixture
+def tokenizer():
+    BPETokenizer.set_vocabulary_size(260)
+    return BPETokenizer.train(["a cat with a hat eats the hat cat"])
+
+def test_encode_decode(tokenizer):
+    test_text = "brat the mat"
+    encoded = tokenizer.encode(test_text)
+    decoded = tokenizer.decode(encoded)
+
+    assert decoded == test_text
+
+def test_merge_rules(tokenizer):
+    result = tokenizer._apply_merge_rule([2,3,5,67,45],[5,67,90])
+    assert result == [2,3,90,45]
