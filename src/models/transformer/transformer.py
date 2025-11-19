b5dc08b0395d6fe20c561fccd68f1c8c5f145645
Till <till.flotow@gmx.de>
2025-11-19 09:28:44 +0100

    [feature/trainsformer_skeleton] added skeleton

---
+++ src/models/transformer/transformer.py
@@ -0,0 +1,13 @@
+import torch.nn as nn
+
+
+class Transformer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self):
+        pass
+
+    def generate(self):
+        pass
