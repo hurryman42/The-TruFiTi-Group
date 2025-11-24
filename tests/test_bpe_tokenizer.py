a2b685a49c16f6c7d519927a9a0d8abcd02d370e
TRUNG_mainpc <nguy.hoangtrung@gmail.com>
2025-11-24 19:23:39 +0100

    small test fix

--- tests/test_bpe_tokenizer.py
+++ tests/test_bpe_tokenizer.py
@@ -16,3 +16,22 @@ def test_encode_decode(tokenizer):
 def test_merge_rules(tokenizer):
     result = tokenizer._apply_merge_rule([2,3,5,67,45],[5,67,90])
     assert result == [2,3,90,45]
+
+def test_save_and_load(tokenizer, tmp_path):
+    # Use tmp_path to create a temporary directory
+    temp_dir = tmp_path / "my_temp_dir"
+    temp_dir.mkdir()
+
+    temp_file = temp_dir / "test_file.txt"
+
+    tokenizer.save(temp_file)
+
+    tokenizer2 = BPETokenizer.load(temp_file)
+
+    print("TEST:", tokenizer2._vocabulary)
+
+    test_text = "brat the mat"
+    encoded = tokenizer2.encode(test_text)
+    decoded = tokenizer2.decode(encoded)
+
+    assert decoded == test_text
